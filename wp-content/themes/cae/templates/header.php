98b5976b7a6b79b3bf434883b899cc09fb362183
Jon Bukiewicz <jonathan@tenebroso.net>
2014-09-13 12:53:03 -0500

    Setting up header HTML

--- wp-content/themes/cae/templates/header.php
+++ wp-content/themes/cae/templates/header.php
@@ -1,21 +1,20 @@
-<nav class="nav-main">
-	<a class="brand" href="<?php echo home_url(); ?>/"><img src="<?php echo img_dir(); ?>logo.svg"></a>
-	<?php
+<header id="fixed" class="banner" role="banner">
+  <div class="container">
+  
+    <a class="brand" href="<?php echo home_url(); ?>/"><img src="<?php echo img_dir(); ?>svg/logo.svg"></a>
+    
+    <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav hidden utility-nav'));
       endif;
     ?>
-</nav>
 
-<header class="banner" role="banner">
-<!--   <div class="container">
-  
-        <?php
-          if (has_nav_menu('primary_navigation')) :
-            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav utility-nav'));
-          endif;
-        ?>
-    
-  </div> -->
-   <?php get_template_part('templates/page', 'header'); ?>
+	<div class="pull-right">
+    	<?php get_search_form();?>
+    	<a class="js-nav-trigger nav-collapse">
+
+    	</a>
+    </div>
+
+  </div>
 </header>
