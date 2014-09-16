b926c81a52e7993e9112ecb614457be875eacb0f
Jon Bukiewicz <jonathan@tenebroso.net>
2014-09-16 09:27:33 -0500

    Front page beginnings

--- wp-content/themes/cae/page.php
+++ wp-content/themes/cae/page.php
@@ -1,14 +1,2 @@
-<?php while (have_posts()) : the_post(); ?>
-	<div class="container">
-
-		<div class="row">
-
-			<div class="col-xs-12">
-					 <?php get_template_part('templates/content', 'page'); ?>
-			</div>
-
-		</div>
-
-	</div>
- 
-<?php endwhile; ?>
+<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/content', 'page'); ?>
