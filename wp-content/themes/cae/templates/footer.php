51f1da582cb6f54f5686dbf907884a349b7d68a1
Jon Bukiewicz <jonathan@tenebroso.net>
2014-09-16 16:02:57 -0500

    Lots of work on footer

--- wp-content/themes/cae/templates/footer.php
+++ wp-content/themes/cae/templates/footer.php
@@ -1,14 +1,48 @@
 <footer class="content-info footer" role="contentinfo">
-  <div class="container">
-    <div class="col-xs-3">
+
+  <div class="footer-cta-wrap">
+
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-md-3 nb-cta">
+          <h3 class="nb-cta-text">Count Me In</h3>
+        </div>
+        <div class="col-md-9 nb-cta-form">
+          <input type="email" placeholder="Email">
+          <input type="text" placeholder="Zipcode">
+          <input type="submit" value="Submit">
+        </div>
+
+      </div>
+
+      <div class="row">
+
+        <div class="col-xs-12">
+          
+          <ul class="nav footer-social-icons">
+            <li class="icon-twitter"> </li>
+            <li class="icon-facebook"> </li>
+            <li class="icon-youtube"> </li>
+            <li class="icon-instagram"> </li>
+          </ul>
+
+        </div>
+
+      </div>
 
     </div>
-    <div class="col-xs-9">
-		<?php
-          if (has_nav_menu('footer')) :
-            wp_nav_menu(array('theme_location' => 'footer', 'menu_class' => 'nav footer-nav'));
-          endif;
-        ?>
+
+  </div>
+
+  <div class="container">
+    <div class="col-md-4 footer-logo">
+      <img src="<?php echo img_dir(); ?>/svg/logo-footer.svg">
+      <p class="footer-copyright">&copy; Copyright <?php echo date('Y'); ?> <?php bloginfo('title'); ?></p>
+    </div>
+    <div class="col-md-8">
+		
     </div>
   </div>
 </footer>
--- wp-content/themes/cae/templates/head.php
+++ wp-content/themes/cae/templates/head.php
@@ -13,4 +13,9 @@
 	<![endif]-->
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
+<script>
+window.caeicons=function(e){if(e&&3===e.length){var t=window,n=!(!t.document.createElementNS||!t.document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect||!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1")||window.opera&&-1===navigator.userAgent.indexOf("Chrome")),o=function(o){var r=t.document.createElement("link"),a=t.document.getElementsByTagName("script")[0];r.rel="stylesheet",r.href=e[o&&n?0:o?1:2],a.parentNode.insertBefore(r,a)},r=new t.Image;r.onerror=function(){o(!1)},r.onload=function(){o(1===r.width&&1===r.height)},r.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="}};
+caeicons( [ "/assets/icons.data.svg.css", "/assets/icons.data.png.css", "/assets/icons.fallback.css" ] );
+</script>
+<noscript><link href="/assets/icons.fallback.css" rel="stylesheet"></noscript>
 </head>
