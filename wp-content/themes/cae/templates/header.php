ab5ba739c0842fbbbe8043fc2f87a0a3425dfe72
Jon Bukiewicz <jonathan@tenebroso.net>
2014-09-16 16:02:12 -0500

    Update logo location

--- wp-content/themes/cae/templates/header.php
+++ wp-content/themes/cae/templates/header.php
@@ -1,7 +1,7 @@
 <header id="fixed" class="banner" role="banner">
   <div class="container">
   
-    <a class="brand" href="<?php echo home_url(); ?>/"><img src="<?php echo img_dir(); ?>svg/logo.svg"></a>
+    <a class="brand" href="<?php echo home_url(); ?>/"><img src="<?php echo img_dir(); ?>/svg/logo.svg"></a>
     
     <?php
       if (has_nav_menu('primary_navigation')) :
