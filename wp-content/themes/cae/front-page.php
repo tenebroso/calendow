a2ffaf4fb1b3fa22b6baf23557dbd01f63243635
Jon Bukiewicz <jonathan@tenebroso.net>
2014-09-18 14:40:57 -0500

    Setup homepage billboard message & billboard nav

--- wp-content/themes/cae/front-page.php
+++ wp-content/themes/cae/front-page.php
@@ -1,3 +1,26 @@
 <?php while (have_posts()) : the_post(); ?>
-Front page!
+	<div class="hero-container">
+
+		<ul class="hero-nav">
+
+			<li class="hero-nav-intro">Building Healthy <br />Communities</li>
+			<li class="neighborhoods"><a href="#">Neighborhoods. <span class="hero-nav-color-block">Neighborhoods.</span></a></li>
+			<li class="prevention"><a href="#">Prevention. <span class="hero-nav-color-block">Prevention.</span></a></li>
+			<li class="schools"><a href="#">School. <span class="hero-nav-color-block">School.</span></a></li>
+			<li class="places"><a href="#">Places. <span class="hero-nav-color-block">Places.</span></a></li>
+			<li class="grants"><a href="#">Grants. <span class="hero-nav-color-block">Grants.</span></a></li>
+			<li class="action"><a href="#">Action. <span class="hero-nav-color-block">Action.</span></a></li>
+
+		</ul>
+
+		<div class="hero-container-text">
+
+			<h2 class="hero-lead">Health and <br />Justice for All.</h2>
+			<p class="hero-subtitle"><span class="hero-subtitle-text">Donec luctur vitae libero sit amet <br />finibus. Lorem ipsum dolor sit amet.</span> <span class="hero-subtitle-arrow icon-lg-arrow"></span></p>
+
+		</div>
+
+		
+
+	</div>
 <?php endwhile; ?>
