00830adad1f6b58552ed7a02eaa6adc1824afa2a
Jon Bukiewicz <jonathan@tenebroso.net>
2014-10-07 11:35:57 -0500

    Scroll to top of site when nav opens

--- wp-content/themes/cae/assets/js/site.js
+++ wp-content/themes/cae/assets/js/site.js
@@ -42,18 +42,21 @@ var CE = CE || {};
   	var $openTrigger = $('.js-nav-open-trigger'),
   		$closeTrigger = $('.js-nav-close-trigger'),
   		$menu = $('.nav-full-screen'),
+      $page = $('html'),
   		$body = $('body:after');
 
     $openTrigger.click(function(){
     	$menu.addClass('opened');
     	$body.addClass('faded');
     	$closeTrigger.addClass('active');
+      $('html,body').scrollTo(0,0);
     });
 
     $closeTrigger.click(function(){
     	$(this).removeClass('active');
     	$menu.removeClass('opened');
     	$body.removeClass('faded');
+      $page.css('min-height','');
     });
 
 
