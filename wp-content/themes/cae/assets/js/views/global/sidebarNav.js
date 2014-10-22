5223a758a9fc6a9a65cfb432b243e6798a5b6ab4
Jon Bukiewicz <jonathan@tenebroso.net>
2014-10-22 18:28:18 -0500

    Setup conference detail placeholder

--- wp-content/themes/cae/assets/js/views/global/sidebarNav.js
+++ wp-content/themes/cae/assets/js/views/global/sidebarNav.js
@@ -5,13 +5,13 @@ var CE = CE || {};
   CE.sidebarNav = function() {
 
     var $nav = $('.side-nav-container');
-    var $container = $('.page-header.text-right .page-title').width();
+    var $container = $('.container').width();
     var $width = $(window).width();
     var $position = ($width - $container) / 2;
     var $sideNav = $('.side-nav li');
 
     if($sideNav.length) {
-      $nav.css('left', $position + 15).transit({opacity:1});
+      $nav.css('left', $position).transit({opacity:1});
       $nav.stickyNavbar({
         animateCSS: false
       });
