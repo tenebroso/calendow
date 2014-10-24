580d902a194e29afac7b53b3c117a310190ff096
Jon Bukiewicz <jonathan@tenebroso.net>
2014-10-24 18:32:42 -0500

    Newsletter header nav

--- wp-content/themes/cae/templates/newsletter/header-nav.php
+++ wp-content/themes/cae/templates/newsletter/header-nav.php
@@ -1,18 +1,22 @@
+<?php $gmt_timestamp = get_post_time('U', true); ?>
 <ul class="newsletters nav">
 	<li class="pull-left"><a href="#" class="icon-lg-arrow text-hide">Previous</a></li>
-	<?php if ( false === ( $newsletters = get_transient( 'newsletters' ) ) ) {
-		$args = array(
-			'post_type' => 'newsletter',
-			'posts_per_page' => 4,
-		);
-		$newsletters = new WP_Query($args);
-		set_transient( 'our_work', $newsletters, 12 * HOUR_IN_SECONDS );
-	} ?>
+	<?php
+    global $post;
+    $current_post = $post;
 
-	<?php if ( $newsletters->have_posts() ) : while ( $newsletters->have_posts() ) : $newsletters->the_post(); ?><li>
-			<a class="active" href="<?php the_permalink(); ?>">
-				<img src="<?php the_field('thumbnail_image'); ?>">
-			</a>
-		</li><?php endwhile; endif; wp_reset_postdata(); ?>
+    for($i = 1; $i <= 3; $i++):
+    $post = get_next_post();
+    setup_postdata($post); 
+    $img = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' ); 
+    $url = get_permalink(); 
+    if($img): ?><li>
+		<a href="<?php echo $url; ?>">
+			<img src="<?php echo $img[0]; ?>">
+		</a>
+    </li><?php endif; endfor;
+    $post = $current_post; 
+
+?>
 	<li class="pull-right"><a href="#" class="icon-lg-arrow text-hide">Next</a></li>
 </ul>
