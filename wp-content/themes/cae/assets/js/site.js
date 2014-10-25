c0c85b9fdf1f28379d18862e698af13cfa01a026
Jon Bukiewicz <jonathan@tenebroso.net>
2014-10-25 08:44:10 -0500

    Finish header nav setup for newsletters

--- wp-content/themes/cae/assets/js/site.js
+++ wp-content/themes/cae/assets/js/site.js
@@ -2,6 +2,39 @@ var CE = CE || {};
 
 ;(function() {
 
+CE.desktopFilter = function() {
+
+	var $parentFilters = $('.filters > li > a');
+
+    $parentFilters.click(function(){
+    	$parentFilters.not(this).parent('li').removeClass('open');
+    	$(this).toggleClass('open').parent('li').toggleClass('open');
+    });
+
+	wp.hooks.addAction('facetwp/refresh/dropdown', function($this, facet_name) {
+        var val = [];
+        $this.find('.facetwp-dd-val.selected').each(function() {
+            val.push($(this).attr('data-value'));
+        });
+        FWP.facets[facet_name] = val;
+    });
+ 
+    wp.hooks.addAction('facetwp/ready', function() {
+        $(document).on('click', '.facetwp-dd-val', function() {
+            $(this).toggleClass('selected');
+            var $facet = $(this).closest('.facetwp-facet');
+            FWP.autoload();
+        });
+    });
+
+	};
+
+
+})();
+var CE = CE || {};
+
+;(function() {
+
   CE.collapse = function() {
 
   	$('.collapse').collapse();
@@ -390,39 +423,6 @@ var CE = CE || {};
   });
 
 };
-
-
-})();
-var CE = CE || {};
-
-;(function() {
-
-CE.desktopFilter = function() {
-
-	var $parentFilters = $('.filters > li > a');
-
-    $parentFilters.click(function(){
-    	$parentFilters.not(this).parent('li').removeClass('open');
-    	$(this).toggleClass('open').parent('li').toggleClass('open');
-    });
-
-	wp.hooks.addAction('facetwp/refresh/dropdown', function($this, facet_name) {
-        var val = [];
-        $this.find('.facetwp-dd-val.selected').each(function() {
-            val.push($(this).attr('data-value'));
-        });
-        FWP.facets[facet_name] = val;
-    });
- 
-    wp.hooks.addAction('facetwp/ready', function() {
-        $(document).on('click', '.facetwp-dd-val', function() {
-            $(this).toggleClass('selected');
-            var $facet = $(this).closest('.facetwp-facet');
-            FWP.autoload();
-        });
-    });
-
-	};
 
 
 })();
--- wp-content/themes/cae/templates/newsletter/header-nav.php
+++ wp-content/themes/cae/templates/newsletter/header-nav.php
@@ -1,22 +1,19 @@
 <?php $gmt_timestamp = get_post_time('U', true); ?>
-<ul class="newsletters nav">
-	<li class="pull-left"><a href="#" class="icon-lg-arrow text-hide">Previous</a></li>
-	<?php
+<ul class="newsletters nav"><?php previous_post_link('<li class="pull-left icon-lg-arrow text-hide">%link</li>', '', ''); ?><li class="active"><a><img src="<?php the_field('thumbnail_image');?>"></a></li><?php
     global $post;
     $current_post = $post;
 
     for($i = 1; $i <= 3; $i++):
-    $post = get_next_post();
-    setup_postdata($post); 
-    $img = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' ); 
-    $url = get_permalink(); 
-    if($img): ?><li>
-		<a href="<?php echo $url; ?>">
-			<img src="<?php echo $img[0]; ?>">
-		</a>
-    </li><?php endif; endfor;
+	    $post = get_next_post();
+		if($post):
+	    setup_postdata($post); 
+	    $img = get_field('thumbnail_image');
+	    $url = get_permalink(); ?><li>
+			<a href="<?php echo $url; ?>">
+				<img src="<?php echo $img; ?>">
+			</a>
+	    </li><?php endif; endfor;
     $post = $current_post; 
 
-?>
-	<li class="pull-right"><a href="#" class="icon-lg-arrow text-hide">Next</a></li>
+?><?php next_post_link('<li class="pull-right icon-lg-arrow text-hide">%link</li>', '', ''); ?>
 </ul>
