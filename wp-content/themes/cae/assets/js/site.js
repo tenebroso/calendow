14cd6ebc16b7e6ab0cca1337aa26455f72a14838
Jon Bukiewicz <jonathan@tenebroso.net>
2014-10-28 15:02:41 -0500

    Z-index fix for slider in footer

--- wp-content/themes/cae/assets/js/site.js
+++ wp-content/themes/cae/assets/js/site.js
@@ -2,6 +2,202 @@ var CE = CE || {};
 
 ;(function() {
 
+CE.desktopFilter = function() {
+
+	var $parentFilters = $('.filters > li > a');
+    var $childFilters = $('.sub-filter li a');
+
+    var masOptions = {
+      itemSelector: '.grid-item',
+      gutter:5,
+    };
+
+    $('#ajax-load-more .alm-btn-wrap .more').trigger('click');
+
+    //$('.grid-item').addClass('loaded');
+
+    $parentFilters.click(function(e){
+        e.preventDefault();
+        $parentFilters.not(this).parent('li').removeClass('open');
+        $(this).parent('.filter-main').toggleClass('open').removeClass('selected');
+    });
+
+var $container = $('.grid-container').imagesLoaded( function() {
+      $container.masonry( masOptions );
+      $('.grid-item').addClass('loaded');
+    });
+
+$childFilters.click( function(e) {
+
+        var $text = $(this).text();
+
+        $container.fadeOut();
+
+        $('.filter-key.reset').hide();
+        $('.filter-key:last-of-type').show();
+
+        
+
+        e.preventDefault();
+
+        var selected_taxonomy, taxonomy_type, taxonomy_name;
+
+        if($(this).parents('li').hasClass('selected')) {
+            var selected_taxonomy = '';
+            var taxonomy_type = '';
+            $(this).parents('li').removeClass('selected');
+        } else {
+            var selected_taxonomy = $(this).attr('title');
+            var taxonomy_type = $(this).parents('ul').data('tax');
+            var taxonomy_name = $(this).children('span').html();
+            $('.filter-main').removeClass('selected');
+            $(this).parents('li').removeClass('open').addClass('selected');
+        }
+
+        data = {
+            action: 'filter_posts',
+            afp_nonce: afp_vars.afp_nonce,
+            tax: taxonomy_type,
+            taxonomy: selected_taxonomy,
+            name: taxonomy_name
+        };
+
+        $.ajax({
+            type: 'post',
+            dataType: 'html',
+            url: afp_vars.afp_ajax_url,
+            data: data,
+            timeout: 5000,
+            success: function( data, textStatus, XMLHttpRequest ) {
+                $container.html( data );
+                console.log(data);
+                $container.fadeIn();
+                $('#ajax-load-more .alm-btn-wrap .more').hide();
+            },
+            error: function( XMLHttpRequest, textStatus, errorThrown ) {
+                console.log( XMLHttpRequest );
+                console.log( textStatus );
+                console.log( errorThrown );
+                console.log('error');
+                $container.html( data);
+                //$container.fadeIn();
+            }
+        });
+
+        $(document).ajaxComplete(function() {
+            $container.masonry('destroy');
+            $container.masonry( masOptions );
+            $('.grid-item').addClass('loaded');
+        });
+
+    });
+
+var masonryInit = true;
+$.fn.almComplete = function(alm){
+        
+        $container.append(alm.el).masonry('appended', alm.el).masonry('layout', masOptions);
+        $('.grid-item').addClass('loaded');
+};
+    
+
+};
+
+
+})();
+var CE = CE || {};
+
+;(function() {
+
+  CE.reportPageHeight = function() {
+
+
+    var $container = $('.single-report .main');
+    var $report = $('.report-single');
+
+
+    // Get an array of all element heights
+    var elementHeights = $report.map(function() {
+      return $(this).height();
+    }).get();
+
+    var maxHeight = Math.max.apply(null, elementHeights);
+
+    $container.height(maxHeight);
+
+
+  };
+
+
+})();
+var CE = CE || {};
+
+;(function() {
+
+  CE.reportSectionSwitcher = function() {
+
+  	var $prev = $('.report-prev a');
+  	var $next = $('.report-next a');
+  	var $reportNavFirst = $('.report-nav li:first-child');
+  	var $reportPanelFirst = $('#report-panel-1');
+  	var $reportNav = $('.report-nav li');
+
+  	$reportNavFirst.addClass('active');
+
+  	$('.report-single:nth-of-type(1) .report-prev a').remove();
+  	$('.report-single:last-of-type .report-next a').remove();
+
+	$next.click(function(e) {
+		var $href = $(this).attr('href');
+		var $parent = $(this).parents('.report-single');
+		var $theID  = $(this).parents('.report-single').data("id");
+		$parent.transit({x:'-1000%'}, 800, 'ease').removeClass('current');
+		$($href).transit({x:0}, 800, 'ease').addClass('current');
+		$reportNav.removeClass('active');
+		$reportNav.filter(function() {
+			return $(this).data('id') === $theID + 1;
+		}).addClass('active');
+		window.location.hash = $theID + 1;
+		e.preventDefault();
+	});
+
+	$prev.click(function(e) {
+		var $href = $(this).attr('href');
+		var $parent = $(this).parents('.report-single');
+		var $theID  = $(this).parents('.report-single').data("id");
+		$parent.transit({x:'1000%'}, 800, 'ease').removeClass('current');
+		$($href).transit({x:0}, 800, 'ease').addClass('current');
+		$reportNav.removeClass('active');
+		$reportNav.filter(function() {
+			return $(this).data('id') === $theID - 1;
+		}).addClass('active');
+		window.location.hash = $theID - 1;
+		e.preventDefault();
+	});
+
+
+	$('body').on('keydown', function(e){
+		if(e.which == 37) { // left     
+			e.preventDefault();
+			$('.report-single.current .report-prev a')
+			.trigger('click');
+		}
+		else if(e.which == 39) {
+			e.preventDefault();
+			$('.report-single.current .report-next a')
+			.trigger('click');
+			
+			//$('.report-single').hasClass('current').children('.report-next a').trigger('click');
+		}
+	});
+
+  };
+
+
+})();
+var CE = CE || {};
+
+;(function() {
+
   CE.collapse = function() {
 
   	$('.collapse').collapse();
@@ -473,202 +669,6 @@ var CE = CE || {};
   });
 
 };
-
-
-})();
-var CE = CE || {};
-
-;(function() {
-
-CE.desktopFilter = function() {
-
-	var $parentFilters = $('.filters > li > a');
-    var $childFilters = $('.sub-filter li a');
-
-    var masOptions = {
-      itemSelector: '.grid-item',
-      gutter:5,
-    };
-
-    $('#ajax-load-more .alm-btn-wrap .more').trigger('click');
-
-    //$('.grid-item').addClass('loaded');
-
-    $parentFilters.click(function(e){
-        e.preventDefault();
-        $parentFilters.not(this).parent('li').removeClass('open');
-        $(this).parent('.filter-main').toggleClass('open').removeClass('selected');
-    });
-
-var $container = $('.grid-container').imagesLoaded( function() {
-      $container.masonry( masOptions );
-      $('.grid-item').addClass('loaded');
-    });
-
-$childFilters.click( function(e) {
-
-        var $text = $(this).text();
-
-        $container.fadeOut();
-
-        $('.filter-key.reset').hide();
-        $('.filter-key:last-of-type').show();
-
-        
-
-        e.preventDefault();
-
-        var selected_taxonomy, taxonomy_type, taxonomy_name;
-
-        if($(this).parents('li').hasClass('selected')) {
-            var selected_taxonomy = '';
-            var taxonomy_type = '';
-            $(this).parents('li').removeClass('selected');
-        } else {
-            var selected_taxonomy = $(this).attr('title');
-            var taxonomy_type = $(this).parents('ul').data('tax');
-            var taxonomy_name = $(this).children('span').html();
-            $('.filter-main').removeClass('selected');
-            $(this).parents('li').removeClass('open').addClass('selected');
-        }
-
-        data = {
-            action: 'filter_posts',
-            afp_nonce: afp_vars.afp_nonce,
-            tax: taxonomy_type,
-            taxonomy: selected_taxonomy,
-            name: taxonomy_name
-        };
-
-        $.ajax({
-            type: 'post',
-            dataType: 'html',
-            url: afp_vars.afp_ajax_url,
-            data: data,
-            timeout: 5000,
-            success: function( data, textStatus, XMLHttpRequest ) {
-                $container.html( data );
-                console.log(data);
-                $container.fadeIn();
-                $('#ajax-load-more .alm-btn-wrap .more').hide();
-            },
-            error: function( XMLHttpRequest, textStatus, errorThrown ) {
-                console.log( XMLHttpRequest );
-                console.log( textStatus );
-                console.log( errorThrown );
-                console.log('error');
-                $container.html( data);
-                //$container.fadeIn();
-            }
-        });
-
-        $(document).ajaxComplete(function() {
-            $container.masonry('destroy');
-            $container.masonry( masOptions );
-            $('.grid-item').addClass('loaded');
-        });
-
-    });
-
-var masonryInit = true;
-$.fn.almComplete = function(alm){
-        
-        $container.append(alm.el).masonry('appended', alm.el).masonry('layout', masOptions);
-        $('.grid-item').addClass('loaded');
-};
-    
-
-};
-
-
-})();
-var CE = CE || {};
-
-;(function() {
-
-  CE.reportPageHeight = function() {
-
-
-    var $container = $('.single-report .main');
-    var $report = $('.report-single');
-
-
-    // Get an array of all element heights
-    var elementHeights = $report.map(function() {
-      return $(this).height();
-    }).get();
-
-    var maxHeight = Math.max.apply(null, elementHeights);
-
-    $container.height(maxHeight);
-
-
-  };
-
-
-})();
-var CE = CE || {};
-
-;(function() {
-
-  CE.reportSectionSwitcher = function() {
-
-  	var $prev = $('.report-prev a');
-  	var $next = $('.report-next a');
-  	var $reportNavFirst = $('.report-nav li:first-child');
-  	var $reportPanelFirst = $('#report-panel-1');
-  	var $reportNav = $('.report-nav li');
-
-  	$reportNavFirst.addClass('active');
-
-  	$('.report-single:nth-of-type(1) .report-prev a').remove();
-  	$('.report-single:last-of-type .report-next a').remove();
-
-	$next.click(function(e) {
-		var $href = $(this).attr('href');
-		var $parent = $(this).parents('.report-single');
-		var $theID  = $(this).parents('.report-single').data("id");
-		$parent.transit({x:'-1000%'}, 800, 'ease').removeClass('current');
-		$($href).transit({x:0}, 800, 'ease').addClass('current');
-		$reportNav.removeClass('active');
-		$reportNav.filter(function() {
-			return $(this).data('id') === $theID + 1;
-		}).addClass('active');
-		window.location.hash = $theID + 1;
-		e.preventDefault();
-	});
-
-	$prev.click(function(e) {
-		var $href = $(this).attr('href');
-		var $parent = $(this).parents('.report-single');
-		var $theID  = $(this).parents('.report-single').data("id");
-		$parent.transit({x:'1000%'}, 800, 'ease').removeClass('current');
-		$($href).transit({x:0}, 800, 'ease').addClass('current');
-		$reportNav.removeClass('active');
-		$reportNav.filter(function() {
-			return $(this).data('id') === $theID - 1;
-		}).addClass('active');
-		window.location.hash = $theID - 1;
-		e.preventDefault();
-	});
-
-
-	$('body').on('keydown', function(e){
-		if(e.which == 37) { // left     
-			e.preventDefault();
-			$('.report-single.current .report-prev a')
-			.trigger('click');
-		}
-		else if(e.which == 39) {
-			e.preventDefault();
-			$('.report-single.current .report-next a')
-			.trigger('click');
-			
-			//$('.report-single').hasClass('current').children('.report-next a').trigger('click');
-		}
-	});
-
-  };
 
 
 })();
