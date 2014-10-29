b444c5609631a883f83ef98467462805fe1ac355
Jon Bukiewicz <jonathan@tenebroso.net>
2014-10-29 17:38:14 -0500

    Fix for #1599 Newsletter - Footer - Handheld - font-size

--- wp-content/themes/cae/templates/newsletter/footer-nav.php
+++ wp-content/themes/cae/templates/newsletter/footer-nav.php
@@ -4,10 +4,10 @@
 
     <div class="container">
       <div class="row">
-		<div class="col-xs-6 footer-post-nav-item footer-post-nav-prev">
+		<div class="col-sm-6 footer-post-nav-item footer-post-nav-prev">
 			<?php previous_post_link('%link', '<p>Previous Story</p> <h2><strong>%title</strong></h2>', ''); ?>
 		</div>
-		<div class="col-xs-6 text-right footer-post-nav-item footer-post-nav-next">
+		<div class="col-sm-6 text-right footer-post-nav-item footer-post-nav-next">
 			<?php next_post_link('%link', '<p>Next Story</p> <h2><strong>%title</strong></h2>', ''); ?>
 		</div>
       </div>
