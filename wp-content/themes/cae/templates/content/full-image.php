b0a4a836cfdd1811e1893db3234201d62fc3009b
Jon Bukiewicz <jonathan@tenebroso.net>
2014-10-30 09:41:52 -0500

    Setup placeholder image - fix alignment

--- wp-content/themes/cae/templates/content/full-image.php
+++ wp-content/themes/cae/templates/content/full-image.php
@@ -1,12 +1,17 @@
 <?php 	$template = get_page_template_slug( $post->ID ); 
-		$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );?>
+		$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+		$custom_image = get_field('full_width_image'); ?>
 
 <?php if(($template == 'page-campaign-detail.php') && $large_image_url): ?>
 <div class="stripe bg-image">
 	<div data-stellar-background-ratio="0.5" class="parallax-image" style="background-image:url(<?php echo $large_image_url[0]; ?>);"></div>
 </div>
-<?php else: ?>
+<?php elseif($custom_image): ?>
 <div class="stripe bg-image">
-	<div data-stellar-background-ratio="0.5" class="parallax-image" style="background-image:url(<?php the_sub_field('full_width_image'); ?>);"></div>
+	<div data-stellar-background-ratio="0.5" class="parallax-image" style="background-image:url(<?php echo $custom_image; ?>);"></div>
+</div>
+<?php else: ?>
+	<div class="stripe bg-image">
+	<div data-stellar-background-ratio="0.5" class="parallax-image" style="background-image:url(//placehold.it/1200x400);"></div>
 </div>
 <?php endif; ?>
