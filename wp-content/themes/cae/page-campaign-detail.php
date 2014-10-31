554c20aac218ffe609fa374f98f9ef9116c425a6
Jon Bukiewicz <jonathan@tenebroso.net>
2014-10-31 07:34:14 -0500

    Fix for #1474 Missing Health Happens Here pin to left of content

--- wp-content/themes/cae/page-campaign-detail.php
+++ wp-content/themes/cae/page-campaign-detail.php
@@ -6,5 +6,14 @@
 </div>
 <div class="col-sm-9">
 	<?php get_template_part('templates/content', 'page'); ?>
+	<?php if( has_term( 'neighborhoods', 'work' ) ) { ?>
+		<div class="health-block icon-health-placemarker-neighborhoods">
+			<p class="placemarker-block caps">Health <br />Happens <br />Here <br /><span class="color">in Neighborhoods</p>
+		</div>
+	<?php } else { ?>
+		<div class="health-block icon-health-placemarker-places">
+			<p class="placemarker-block caps">Health <br />Happens <br />Here <br /><span class="color">in Places</p>
+		</div>
+	<?php } ?>
 </div>
 </div>
