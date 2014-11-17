3cba98f07b34d7468e018d9906d5edf45a68cfe4
Jon Bukiewicz <jonathan@tenebroso.net>
2014-11-16 19:55:51 -0600

    Push fixes to header image and nav close on click of transparent white space

--- wp-content/themes/cae/assets/js/site.js
+++ wp-content/themes/cae/assets/js/site.js
@@ -2,6 +2,131 @@ var CE = CE || {};
 
 ;(function() {
 
+CE.desktopFilter = function() {
+
+	var $parentFilters = $('.filters > li > a');
+    var $childFilters = $('.sub-filter li a');
+    var $ajaxLoadMore = $('#ajax-load-more .alm-btn-wrap .more');
+    var $gridContainer = $('.grid-container');
+    var $gridItem = $('.grid-item');
+
+    var masOptions = {
+      itemSelector: '.grid-item',
+      gutter:5,
+    };
+
+    $ajaxLoadMore.trigger('click');
+
+    $parentFilters.click(function(e){
+        e.preventDefault();
+        $parentFilters.not(this).parent('li').removeClass('open');
+        $(this).parent('.filter-main').toggleClass('open').removeClass('selected');
+    });
+
+    $('.filters > li').hover(function() {
+        $('.filters > li').not(this).children('.filter-key').css('background-image','none');
+    },function(){
+        $('.filter-key').css('background-image','');
+    });
+
+    $(document).mouseup(function (e)
+    {
+        var container = $(".main");
+
+        if (!container.is(e.target) && container.has(e.target).length === 0); // ... nor a descendant of the container
+        {
+            $('.filters li').removeClass('open');
+        }
+    });
+
+var $container = $gridContainer.imagesLoaded( function() {
+      $container.masonry( masOptions );
+      $('.grid-item').addClass('loaded');
+    });
+
+$childFilters.click( function(e) {
+
+        var $text = $(this).text();
+
+        $container.fadeOut();
+
+        $('.filter-key.reset').hide();
+        $('.filter-key:last-of-type').show();
+
+        e.preventDefault();
+
+        var selected_taxonomy, taxonomy_type, taxonomy_name;
+
+        if($(this).parents('li').hasClass('selected')) {
+            var selected_taxonomy = '';
+            var taxonomy_type = '';
+            var taxonomy_name = '';
+            $('.filter-key').css('background-image','');
+            $(this).parents('li').removeClass('selected');
+        } else {
+            var selected_taxonomy = $(this).attr('title');
+            var taxonomy_type = $(this).parents('ul').data('tax');
+            var taxonomy_name = $(this).children('span').html();
+            $('.filter-main').removeClass('selected');
+            $('.filter-key').css('background-image','none');
+            $(this).parents('li').removeClass('open').addClass('selected');
+        }
+
+        console.log(taxonomy_name);
+
+        data = {
+            action: 'filter_posts',
+            afp_nonce: afp_vars.afp_nonce,
+            tax: taxonomy_type,
+            taxonomy: selected_taxonomy,
+            name: taxonomy_name
+        };
+
+        $.ajax({
+            type: 'post',
+            dataType: 'html',
+            url: afp_vars.afp_ajax_url,
+            data: data,
+            timeout: 5000,
+            success: function( data, textStatus, XMLHttpRequest ) {
+                $container.html( data );
+                $container.fadeIn();
+                $('#ajax-load-more .alm-btn-wrap .more').hide();
+            },
+            error: function( XMLHttpRequest, textStatus, errorThrown ) {
+                console.log( XMLHttpRequest );
+                console.log( textStatus );
+                console.log( errorThrown );
+                console.log('error');
+                $container.html( data);
+                //$container.fadeIn();
+            }
+        });
+
+        $(document).ajaxComplete(function() {
+            $container.masonry('destroy');
+            $container.masonry( masOptions );
+            $('.grid-item').addClass('loaded');
+        });
+
+    });
+
+var masonryInit = true;
+$.fn.almComplete = function(alm){
+        
+        $container.append(alm.el).masonry('appended', alm.el).masonry('layout', masOptions);
+        $('.grid-item').addClass('loaded');
+};
+    
+
+};
+
+
+})();
+var CE = CE || {};
+
+;(function() {
+
   CE.collapse = function() {
 
   	$('.collapse').collapse();
@@ -142,7 +267,7 @@ var CE = CE || {};
   CE.navTrigger = function() {
 
   	var $openTrigger = $('.js-nav-open-trigger'),
-  		$closeTrigger = $('.js-nav-close-trigger'),
+  		$closeTrigger = $('.js-nav-close-trigger, .main, .footer'),
   		$menu = $('.nav-full-screen'),
       $menuHeight = $menu.height();
       $page = $('html'),
@@ -151,15 +276,20 @@ var CE = CE || {};
 
     $openTrigger.click(function(){
     	$menu.addClass('opened');
-    	$body.addClass('faded').css('max-height',$menuHeight).css('overflow','hidden');
+    	//$body.addClass('faded').css('min-height',$intViewportHeight);
+      $('.banner').css('display','none');
+      $('.main, .footer').css('z-index','2').transit({opacity:0.2});
     	$closeTrigger.addClass('active');
       $('html, body').animate({scrollTop : 0},200);
+      $
     });
 
     $closeTrigger.click(function(){
     	$(this).removeClass('active');
     	$menu.removeClass('opened');
-    	$body.removeClass('faded').css('min-height','').css('overflow','');
+      $('.banner').css('display','');
+      $('.main, .footer').css('z-index','').transit({opacity:1});
+    	//$body.removeClass('faded').css('min-height','').css('overflow','');
       $page.css('min-height','');
     });
 
@@ -478,131 +608,6 @@ var CE = CE || {};
       $("#tbl").html('78&deg;');
     }
   });
-
-};
-
-
-})();
-var CE = CE || {};
-
-;(function() {
-
-CE.desktopFilter = function() {
-
-	var $parentFilters = $('.filters > li > a');
-    var $childFilters = $('.sub-filter li a');
-    var $ajaxLoadMore = $('#ajax-load-more .alm-btn-wrap .more');
-    var $gridContainer = $('.grid-container');
-    var $gridItem = $('.grid-item');
-
-    var masOptions = {
-      itemSelector: '.grid-item',
-      gutter:5,
-    };
-
-    $ajaxLoadMore.trigger('click');
-
-    $parentFilters.click(function(e){
-        e.preventDefault();
-        $parentFilters.not(this).parent('li').removeClass('open');
-        $(this).parent('.filter-main').toggleClass('open').removeClass('selected');
-    });
-
-    $('.filters > li').hover(function() {
-        $('.filters > li').not(this).children('.filter-key').css('background-image','none');
-    },function(){
-        $('.filter-key').css('background-image','');
-    });
-
-    $(document).mouseup(function (e)
-    {
-        var container = $(".main");
-
-        if (!container.is(e.target) && container.has(e.target).length === 0); // ... nor a descendant of the container
-        {
-            $('.filters li').removeClass('open');
-        }
-    });
-
-var $container = $gridContainer.imagesLoaded( function() {
-      $container.masonry( masOptions );
-      $('.grid-item').addClass('loaded');
-    });
-
-$childFilters.click( function(e) {
-
-        var $text = $(this).text();
-
-        $container.fadeOut();
-
-        $('.filter-key.reset').hide();
-        $('.filter-key:last-of-type').show();
-
-        e.preventDefault();
-
-        var selected_taxonomy, taxonomy_type, taxonomy_name;
-
-        if($(this).parents('li').hasClass('selected')) {
-            var selected_taxonomy = '';
-            var taxonomy_type = '';
-            var taxonomy_name = '';
-            $('.filter-key').css('background-image','');
-            $(this).parents('li').removeClass('selected');
-        } else {
-            var selected_taxonomy = $(this).attr('title');
-            var taxonomy_type = $(this).parents('ul').data('tax');
-            var taxonomy_name = $(this).children('span').html();
-            $('.filter-main').removeClass('selected');
-            $('.filter-key').css('background-image','none');
-            $(this).parents('li').removeClass('open').addClass('selected');
-        }
-
-        console.log(taxonomy_name);
-
-        data = {
-            action: 'filter_posts',
-            afp_nonce: afp_vars.afp_nonce,
-            tax: taxonomy_type,
-            taxonomy: selected_taxonomy,
-            name: taxonomy_name
-        };
-
-        $.ajax({
-            type: 'post',
-            dataType: 'html',
-            url: afp_vars.afp_ajax_url,
-            data: data,
-            timeout: 5000,
-            success: function( data, textStatus, XMLHttpRequest ) {
-                $container.html( data );
-                $container.fadeIn();
-                $('#ajax-load-more .alm-btn-wrap .more').hide();
-            },
-            error: function( XMLHttpRequest, textStatus, errorThrown ) {
-                console.log( XMLHttpRequest );
-                console.log( textStatus );
-                console.log( errorThrown );
-                console.log('error');
-                $container.html( data);
-                //$container.fadeIn();
-            }
-        });
-
-        $(document).ajaxComplete(function() {
-            $container.masonry('destroy');
-            $container.masonry( masOptions );
-            $('.grid-item').addClass('loaded');
-        });
-
-    });
-
-var masonryInit = true;
-$.fn.almComplete = function(alm){
-        
-        $container.append(alm.el).masonry('appended', alm.el).masonry('layout', masOptions);
-        $('.grid-item').addClass('loaded');
-};
-    
 
 };
 
--- wp-content/themes/cae/assets/js/views/global/navTrigger.js
+++ wp-content/themes/cae/assets/js/views/global/navTrigger.js
@@ -5,7 +5,7 @@ var CE = CE || {};
   CE.navTrigger = function() {
 
   	var $openTrigger = $('.js-nav-open-trigger'),
-  		$closeTrigger = $('.js-nav-close-trigger'),
+  		$closeTrigger = $('.js-nav-close-trigger, .main, .footer'),
   		$menu = $('.nav-full-screen'),
       $menuHeight = $menu.height();
       $page = $('html'),
@@ -14,15 +14,20 @@ var CE = CE || {};
 
     $openTrigger.click(function(){
     	$menu.addClass('opened');
-    	$body.addClass('faded').css('max-height',$menuHeight).css('overflow','hidden');
+    	//$body.addClass('faded').css('min-height',$intViewportHeight);
+      $('.banner').css('display','none');
+      $('.main, .footer').css('z-index','2').transit({opacity:0.2});
     	$closeTrigger.addClass('active');
       $('html, body').animate({scrollTop : 0},200);
+      $
     });
 
     $closeTrigger.click(function(){
     	$(this).removeClass('active');
     	$menu.removeClass('opened');
-    	$body.removeClass('faded').css('min-height','').css('overflow','');
+      $('.banner').css('display','');
+      $('.main, .footer').css('z-index','').transit({opacity:1});
+    	//$body.removeClass('faded').css('min-height','').css('overflow','');
       $page.css('min-height','');
     });
 
