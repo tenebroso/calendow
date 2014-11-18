3f0ce74673c8669d70a64fe1e279fb5b66ff91ff
Jon Bukiewicz <jonathan@tenebroso.net>
2014-11-18 06:16:29 -0600

    More homepage grid work

--- wp-content/themes/cae/templates/home/grid.php
+++ wp-content/themes/cae/templates/home/grid.php
@@ -10,11 +10,14 @@
 <div class="grid-item <?php echo $className; ?> <?php if($workSlug): echo strtolower($workSlug[0]); else: echo 'default'; endif; ?>">
 	<a href="<?php the_permalink(); ?>"<?php if($image_url): ?> style="background-image:url(<?php echo $image_url; ?>);"<?php endif; ?>>
 		<?php if($className !== 'grid-video'): ?>
-			<div class="breadcrumbs hover-white">
-				<?php if($work): ?><span class="color hover-white"><?php echo $work[0]; ?></span> <span class="carrot hover-white">></span> <?php endif; ?><?php if($campaign): echo $campaign[0]; endif; ?><strong class="hover-white"><?php echo get_post_type( get_the_ID() ); ?></strong>
-			</div>
-			<h2 class="grid-title hover-white"><?php the_title(); ?></h2>
-			<p class="date hover-white"><?php the_time('F j, Y'); ?></p>
+
+			<?php if(!$image_url): ?>
+				<div class="breadcrumbs hover-white">
+					<?php if($work): ?><span class="color hover-white"><?php echo $work[0]; ?></span> <span class="carrot hover-white">></span> <?php endif; ?><?php if($campaign): echo $campaign[0]; endif; ?><strong class="hover-white"><?php echo get_post_type( get_the_ID() ); ?></strong>
+				</div>
+				<h2 class="grid-title hover-white"><?php the_title(); ?></h2>
+				<p class="date hover-white"><?php the_time('F j, Y'); ?></p>
+			<?php endif; ?>
 		<?php elseif($className == 'grid-video'): ?>
 			<div class="v-centered grid-play-button">Play Video</div>
 		<?php endif; ?>
