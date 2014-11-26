d1887696699d11621933e4ed5eeeeff791313e35
Jon Bukiewicz <jonathan@tenebroso.net>
2014-11-26 07:27:31 -0600

    Add dedicated image space for accordion panels

--- wp-content/themes/cae/templates/content-accordion.php
+++ wp-content/themes/cae/templates/content-accordion.php
@@ -6,7 +6,8 @@
     
       $title = get_sub_field('accordion_title');
       $sub = get_sub_field('accordion_sub-title');
-      $aContent = get_sub_field('accordion_content'); ?>
+      $aContent = get_sub_field('accordion_content'); 
+      $aImage = get_sub_field('accordion_image'); ?>
 
   <div class="panel panel-default">
     <div class="panel-heading">
@@ -19,7 +20,19 @@
     <div id="collapse<?php echo $i; ?>" class="panel-collapse collapse in">
       <div class="panel-body">
         <div class="container text-content">
+        <?php if($aImage): ?>
+          <div class="row">
+            <div class="col-sm-3">
+              <img src="<?php echo $aImage; ?>">
+              <a href="#top" class="btn btn-default">Back to Top</a>
+            </div>
+            <div class="col-sm-9">
+              <?php echo $aContent; ?>
+            </div>
+          </div>
+        <?php else: ?>
           <?php echo $aContent; ?>
+        <?php endif; ?>
         </div>
       </div>
     </div>
