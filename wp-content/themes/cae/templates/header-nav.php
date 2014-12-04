9cfbee79c1cbb484dc47b32ed07e4b3fb47f60a7
Jon Bukiewicz <jonathan@tenebroso.net>
2014-12-04 13:13:23 -0600

    Fix the campaign detail header nav

--- wp-content/themes/cae/templates/header-nav.php
+++ wp-content/themes/cae/templates/header-nav.php
@@ -13,7 +13,6 @@ if ($workSlug[0] == 'places') { ?>
 		<li class="active"><a class="resize-thumb"><?php the_field('name_abbreviation'); ?></a></li><?php
 		for($i = 1; $i <= 3; $i++):
 		    $post = get_next_post();
-			if(!$post): $post = get_previous_post(); endif;
 			if($post):
 		    setup_postdata($post);
 		    $url = get_permalink(); ?><li>
@@ -27,17 +26,20 @@ if ($workSlug[0] == 'places') { ?>
 	<p class="sidenav-title bg-color section-intro-title"><?php echo $workSlug[0]; ?>.</p>
 	<ul class="newsletters bg-color sub-nav nav"><?php previous_post_link('<li class="pull-left icon-lg-arrow text-hide">%link</li>', '', ''); ?>
 		<li class="active"><a class="resize-thumb"><img src="<?php echo $image; ?>"></a></li><?php
-	    for($i = 1; $i <= 3; $i++):
-		    $post = get_next_post();
-			if(!$post): $post = get_previous_post(); endif;
-			if($post):
-		    setup_postdata($post); 
-			$image = get_field('header_thumbnail_nav_image');
-		    $url = get_permalink(); ?><li>
-				<a href="<?php echo $url; ?>" class="resize-thumb">
-					<img src="<?php echo $image; ?>">
-				</a>
-		    </li><?php endif; endfor;
+		    $parent =  $post->post_parent;
+		    $mypages = get_pages( array( 'child_of' => $parent, 'exclude' => $post->ID, 'sort_order' => 'desc' ) );
+		    $i = 1;
+		    foreach( $mypages as $page ) {
+
+				$content = $page->post_content;
+				if ( $i <= 4 ):
+				 	
+					$image = get_field('header_thumbnail_nav_image', $page->ID); if(!$image): $image = 'http://placehold.it/86x81'; endif; ?><li>
+						<a href="<?php echo get_page_link( $page->ID ); ?>" class="resize-thumb">
+							<img src="<?php echo $image; ?>">
+						</a>
+					</li><?php endif; $i++;
+			}
 	}
 	
 	$post = $current_post; 
