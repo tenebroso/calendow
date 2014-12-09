ac42756c95b9a3c65184007d11f91c2ee9e7ca43
Jon Bukiewicz <jonathan@tenebroso.net>
2014-12-09 17:24:09 -0600

    Add php check for image height on homepage grid

--- wp-content/themes/cae/templates/home/grid.php
+++ wp-content/themes/cae/templates/home/grid.php
@@ -1,13 +1,14 @@
 <?php 	
 		$className = '';
-		$image_url = get_field('tile_image');;
+		$image_url = get_field('tile_image');
+		list($width, $height, $type, $attr) = getimagesize($image_url);
 		$work = wp_get_post_terms($post->ID,'work', array("fields" => "names"));
 		$campaign = wp_get_post_terms($post->ID,'campaign', array("fields" => "names"));
 		$workSlug = wp_get_post_terms($post->ID,'work', array("fields" => "slugs"));
 		$className = 'grid-'.get_post_type().""; ?>
 
 
-<div class="grid-item <?php echo $className; ?> <?php if($workSlug): echo strtolower($workSlug[0]); else: echo 'default'; endif; ?>">
+<div class="grid-item <?php echo $className; ?> <?php if($workSlug): echo strtolower($workSlug[0]); else: echo 'default'; endif; ?>"<?php if($image_url):?>style="height:<?php echo $height; endif; ?>">
 	<a href="<?php the_permalink(); ?>"<?php if($image_url): ?> class="bg-color hover-trigger" style="background-image:url(<?php echo $image_url; ?>);"<?php endif; ?>>
 		<?php if($className !== 'grid-video'): ?>
 
