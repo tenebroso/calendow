72236536abbf72e360b92eae8cc046fb3d6dcd11
Jon Bukiewicz <jonathan@tenebroso.net>
2015-01-26 10:48:59 -0600

    Add start of resources page

---
+++ wp-content/themes/cae/page-resources.php
@@ -0,0 +1,8 @@
+<div class="row">
+<div class="col-sm-2">
+	<?php get_template_part('templates/sidebar-share'); ?>
+</div>
+<div class="col-sm-9 text-content">
+	<?php get_template_part('templates/content', 'page'); ?>
+</div>
+</div>
