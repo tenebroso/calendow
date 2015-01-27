6ad75652c880133f25b054016a4bfc55bc383aee
Jon Bukiewicz <jonathan@tenebroso.net>
2015-01-27 13:58:33 -0600

    Addition of rudimentary resources page

--- wp-content/themes/cae/page-resources.php
+++ wp-content/themes/cae/page-resources.php
@@ -2,7 +2,33 @@
 <div class="col-sm-2">
 	<?php get_template_part('templates/sidebar-share'); ?>
 </div>
-<div class="col-sm-9 text-content">
-	<?php get_template_part('templates/content', 'page'); ?>
+<div class="col-sm-9 text-content resources-container">
+
+	<div class="row">
+	<?php if( have_rows('logos') ): ?>
+		<div class="col-md-4">
+			<h3>Logos</h3>
+			<?php while ( have_rows('logos') ) : the_row(); ?>    
+		        <?php the_sub_field('logo'); ?>
+			<?php endwhile; ?>
+		</div>
+	<?php else : endif; ?>
+	<?php if( have_rows('videos') ): ?>
+		<div class="col-md-4">
+			<h3>Videos</h3>
+			<?php while ( have_rows('videos') ) : the_row(); ?>    
+		        <?php the_sub_field('video'); ?>
+			<?php endwhile; ?>
+		</div>
+	<?php else : endif; ?>
+	<?php if( have_rows('documents') ): ?>
+		<div class="col-md-4">
+			<h3>Videos</h3>
+			<?php while ( have_rows('documents') ) : the_row(); ?>    
+		        <?php the_sub_field('document'); ?>
+			<?php endwhile; ?>
+		</div>
+	<?php else : endif; ?>
+	</div>
 </div>
 </div>
