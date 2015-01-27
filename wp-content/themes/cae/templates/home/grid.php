c3b186b625f7ac85a9042daae7e23ce44b2f6c1a
Jon Bukiewicz <jonathan@tenebroso.net>
2015-01-27 08:21:40 -0600

    Add breadcrumbs to photo posts

--- wp-content/themes/cae/templates/home/grid.php
+++ wp-content/themes/cae/templates/home/grid.php
@@ -8,7 +8,7 @@
 		$className = 'grid-'.get_post_type().""; ?>
 
 
-<div class="grid-item <?php echo $className; ?> <?php if($workSlug): echo strtolower($workSlug[0]); else: echo 'default'; endif; ?>"<?php if($image_url):?>style="height:<?php echo $height; ?>px;" <?php endif; ?>">
+<div class="grid-item <?php echo $className; ?> <?php if($workSlug): echo strtolower($workSlug[0]); else: echo 'default'; endif; ?>"<?php if($image_url):?>style="height:<?php echo $height; ?>px;" <?php endif; ?>>
 	<a href="<?php the_permalink(); ?>"<?php if($image_url): ?> class="bg-color hover-trigger" style="background-image:url(<?php echo $image_url; ?>);"<?php endif; ?>>
 		<?php if($className !== 'grid-video'): ?>
 
@@ -20,7 +20,10 @@
 				<p class="date hover-white"><?php the_time('F j, Y'); ?></p>
 			<?php else: ?>
 				<div class="bg-color show-on-hover">
-					<h2 class="grid-title hover-white white-text v-centered"><?php the_title(); ?></h2>
+					<div class="breadcrumbs hover-white">
+						<?php if($work): ?><span class="color hover-white"><?php echo $work[0]; ?></span> <span class="carrot hover-white">></span> <?php endif; ?><?php if($campaign): echo $campaign[0]; endif; ?><strong class="hover-white"><?php echo get_post_type( get_the_ID() ); ?></strong>
+					</div>
+					<h2 class="grid-title hover-white white-text"><?php the_title(); ?></h2>
 				</div>
 			<?php endif; ?>
 		<?php elseif($className == 'grid-video'): ?>
