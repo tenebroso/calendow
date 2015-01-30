7130a8b3cd9d8f8058516710a9f5c27a79df0095
Jon Bukiewicz <jonathan@tenebroso.net>
2015-01-30 14:52:57 -0600

    Fixes to places header nav

--- wp-content/themes/cae/templates/header-nav.php
+++ wp-content/themes/cae/templates/header-nav.php
@@ -10,21 +10,18 @@
 
 if ($workSlug[0] == 'places') { ?>
 
-	<ul class="newsletters bg-color sub-nav nav"><?php previous_post_link('<li class="pull-left icon-lg-arrow text-hide">%link</li>', '', ''); ?>
+	<ul class="newsletters bg-color sub-nav nav"><?php previous_post_link('<li class="pull-left icon-lg-arrow text-hide">%link</li>', '', TRUE, '', 'work'); ?>
 
-		    <li class="active"><a class="resize-thumb"><?php the_field('name_abbreviation'); ?></a></li><?php $args = array(
-		    		'post_type' => 'page',
-		    		'post_parent' => 109,
-		    		'posts_per_page' => -1,
-		    		'orderby' => 'title',
-		    		'order' => 'ASC',
-		    		'post__not_in' => array($current)
-		    	); 
-		    $the_query = new WP_Query( $args ); if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); $url = get_permalink(); ?><li>
-				<a href="<?php echo $url; ?>" class="resize-thumb">
+		    <li class="active"><a class="resize-thumb"><?php the_field('name_abbreviation'); ?></a></li><?php for($i = 1; $i <= 99; $i++):
+			    $post = get_next_post(TRUE,'','work');
+				if($post):
+			    setup_postdata($post); 
+			    $image = get_field('header_thumbnail_nav_image'); if(!$image): $image = 'http://placehold.it/86x81'; endif; ?><li>
+						<a href="<?php the_permalink(); ?>" class="resize-thumb">
 					<?php the_field('name_abbreviation'); ?>
 				</a>
-		    </li><?php endwhile; endif; wp_reset_postdata(); ?>
+					</li><?php endif; endfor;
+		    $post = $current_post; ?>
 
 		    		<!--<?php
 		for($i = 1; $i <= 3; $i++):
