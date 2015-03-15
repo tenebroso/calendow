59330efd1720dad473dc75faf57202536cc2e4ba
Jon Bukiewicz <jonathan@tenebroso.net>
2015-03-15 16:36:59 -0500

    Fix to sitemap logic to pull in correct menus

--- wp-content/themes/cae/page-sitemap.php
+++ wp-content/themes/cae/page-sitemap.php
@@ -5,40 +5,54 @@
      <div class="container">
 
 	    <div class="row">
-	      <div class="col-sm-4">
-	          <?php wp_nav_menu( array('menu' => 'Neighborhoods','menu_class' => 'nav-list neighborhoods' )); ?>
-	      </div>
-	      <div class="col-sm-4">
-	          <?php wp_nav_menu( array('menu' => 'Prevention','menu_class' => 'nav-list prevention' )); ?>
-	      </div>
-	      <div class="col-sm-4">
-	         <?php wp_nav_menu( array('menu' => 'Schools','menu_class' => 'nav-list schools' )); ?>
-	      </div>
-	    </div>
+          <div class="col-sm-4">
+              <?php wp_nav_menu( array('menu' => 'Neighborhoods','menu_class' => 'nav-list neighborhoods' )); ?>
+          </div>
+          <div class="col-sm-4">
+              <?php wp_nav_menu( array('menu' => 'Prevention','menu_class' => 'nav-list prevention' )); ?>
+          </div>
+          <div class="col-sm-4">
+             <?php wp_nav_menu( array('menu' => 'Schools','menu_class' => 'nav-list schools' )); ?>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-4">
+              <?php wp_nav_menu( array('menu' => 'Places','menu_class' => 'nav-list places' )); ?>
+          </div>
+          <div class="col-sm-4">
+              <?php wp_nav_menu( array('menu' => 'Grants and Funding','menu_class' => 'nav-list grants-and-funding' )); ?>
+          </div>
+          <div class="col-sm-4">
+              <?php wp_nav_menu( array('menu' => 'Learning','menu_class' => 'nav-list youth-in-action' )); ?>
+          </div>
+        </div><!-- .row -->
 
 	    <div class="row">
-	      <div class="col-sm-4">
-	          <?php wp_nav_menu( array('menu' => 'Places','menu_class' => 'nav-list places' )); ?>
-	      </div>
-	      <div class="col-sm-4">
-	          <?php wp_nav_menu( array('menu' => 'Grants and Funding','menu_class' => 'nav-list grants-and-funding' )); ?>
-	      </div>
-	      <div class="col-sm-4">
-	          <?php wp_nav_menu( array('menu' => 'Action','menu_class' => 'nav-list youth-in-action' )); ?>
-	      </div>
-	    </div><!-- .row -->
+            <div class="col-sm-4">
+                <?php wp_nav_menu( array('menu' => 'Grey Left','menu_class' => 'nav-list secondary' )); ?>
+            </div>
+            <div class="col-sm-4">
+                <?php wp_nav_menu( array('menu' => 'Grey Center','menu_class' => 'nav-list secondary' )); ?>
+            </div>
+            <div class="col-sm-4">
+                <?php wp_nav_menu( array('menu' => 'Grey Right','menu_class' => 'nav-list secondary' )); ?>
+            </div>
+          </div><!-- .row -->
 
 	      <div class="row">
-	        <div class="col-sm-4">
-	            <?php wp_nav_menu( array('menu' => 'Our Story','menu_class' => 'nav-list secondary' )); ?>
-	        </div>
-	        <div class="col-sm-4">
-	            <?php wp_nav_menu( array('menu' => 'Youth in Action','menu_class' => 'nav-list secondary' )); ?>
-	        </div>
-	        <div class="col-sm-4">
-	            <?php wp_nav_menu( array('menu' => 'Newsletters','menu_class' => 'nav-list secondary' )); ?>
-	        </div>
-	      </div><!-- .row -->
+              <div class="col-sm-6">
+                  <?php wp_nav_menu( array('menu' => 'Green Left','menu_class' => 'nav-list' )); ?>
+              </div>
+              <div class="col-sm-6">
+
+                <?php wp_nav_menu( array('menu' => 'Green Center','menu_class' => 'nav-list' )); ?>
+
+              </div>
+              <div class="col-sm-4">
+                  <?php //the_field('contact_information','options'); ?>
+              </div>
+            </div><!-- .row -->
 
 	</div>
 
