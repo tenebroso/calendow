79e5d922368f9ab6a678b420ecf9404e4d8868ad
Jon Bukiewicz <jonathan@tenebroso.net>
2015-03-15 14:55:26 -0500

    Set homepage hero nav to be editable, make <h1> color darker

--- wp-content/themes/cae/templates/home/hero.php
+++ wp-content/themes/cae/templates/home/hero.php
@@ -1,21 +1,40 @@
 <div class="hero-container">
 
 	<ul class="hero-nav">
+        
+<?php $count = 1; if( have_rows('hero_navigation_builder') ):
+    while ( have_rows('hero_navigation_builder') ) : the_row(); ?>
+        
+        <?php $label = get_sub_field('label'); ?>
+        <?php $url = get_sub_field('url'); ?>
+        <?php $color = get_sub_field('color_scheme'); ?>
+        <?php $term = get_term( $color, 'work' ); ?>
+        <?php $slug = $term->slug; ?>
+        
+        <?php if($count == 1): ?>
+        
+        <li class="hero-nav-intro"><a href="<?php echo $url; ?>"><?php echo $label; ?></a></li>
 
-		<li class="hero-nav-intro"><a href="/building-healthy-communities/">Building Healthy <br />Communities</a></li>
-		<li class="neighborhoods"><a href="/neighborhoods/"><span>Neighborhoods.</span> <span class="hero-nav-color-block">Neighborhoods.</span></a></li>
-		<li class="prevention"><a href="/prevention/"><span>Prevention.</span> <span class="hero-nav-color-block">Prevention.</span></a></li>
-		<li class="schools"><a href="/school/"><span>Schools.</span> <span class="hero-nav-color-block">Schools.</span></a></li>
-		<li class="places"><a href="/places/"><span>Places.</span> <span class="hero-nav-color-block">Places.</span></a></li>
-		<li class="grants"><a href="/grants/"><span>Grants.</span> <span class="hero-nav-color-block">Grants.</span></a></li>
-		<li class="action"><a href="/youth-in-action/"><span>Youth in Action.</span> <span class="hero-nav-color-block">Youth in Action.</span></a></li>
-		<li class="default"><a href="/centers/"><span>Centers.</span> <span class="hero-nav-color-block">Centers.</span></a></li>
+        <?php else: ?>
+        
+        <li class="<?php echo $slug; ?>"><a href="<?php echo $url; ?>"><span><?php echo $label; ?></span> <span class="hero-nav-color-block"><?php echo $label; ?></span></a></li>
+       
+       <?php endif; ?>
+        
+
+    <?php $count++; endwhile;
+
+        else :
+
+            echo '<p>Please add hero nav items in the homepage editor</p>';
+
+        endif; ?>
 
 	</ul>
 
 	<div class="hero-container-text">
 
-		<a href="#">
+		<a href="<?php the_field('hero_url'); ?>">
 
 			<h2 class="hero-lead"><?php the_field('hero_image_headline'); ?></h2>
 			<p class="hero-subtitle"><span class="hero-subtitle-text"><?php the_field('hero_image_text'); ?></span> <span class="hero-subtitle-arrow icon-lg-arrow"></span></p>
