83e072b90b9579e8ea9b471f3c845246d03e0c3f
Jon Bukiewicz <jonathan@tenebroso.net>
2015-03-19 06:25:12 -0500

    Add Content Map ID to post column for using in shortcode

--- wp-content/themes/cae/lib/config.php
+++ wp-content/themes/cae/lib/config.php
@@ -391,6 +391,25 @@ function content_map_shortcode($atts) {
 add_shortcode("content-map", "content_map_shortcode");
 
 /* =============================================================================
+   Add Content Map ID to post column for using in shortcode
+   ========================================================================== */
+
+add_filter( 'manage_content-map_posts_columns', 'revealid_add_id_column', 5 );
+add_action( 'manage_content-map_posts_custom_column', 'revealid_id_column_content', 5, 2 );
+
+
+function revealid_add_id_column( $columns ) {
+   $columns['revealid_id'] = 'ID';
+   return $columns;
+}
+
+function revealid_id_column_content( $column, $id ) {
+  if( 'revealid_id' == $column ) {
+    echo $id;
+  }
+}
+
+/* =============================================================================
    Homepage AJAX filtering
    ========================================================================== */
 
