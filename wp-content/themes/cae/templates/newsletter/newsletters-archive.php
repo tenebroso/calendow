9918168f31437230aba5a885db37c6a2955e07db
Jon Bukiewicz <jonathan@tenebroso.net>
2015-03-20 13:53:21 -0500

    Clean up newsletter grid posts and setup the preloading of posts

--- wp-content/themes/cae/templates/newsletter/newsletters-archive.php
+++ wp-content/themes/cae/templates/newsletter/newsletters-archive.php
@@ -1,7 +1,5 @@
 <ul class="newsletter-archives">
 
-	<?php echo do_shortcode('[ajax_load_more repeater="repeater2" post_type="newsletter" posts_per_page="9" scroll="false" pause="true" button_label="Load More"]'); ?>
+	<?php echo do_shortcode('[ajax_load_more repeater="repeater2" preloaded="true" preloaded_amount="9" post_type="newsletter" posts_per_page="9" scroll="false" pause="true" button_label="Load More"]'); ?>
 
 </ul>
-
-<p class="text-center">Please add more newsletters in the backend for proper testing of load more functionality</p>
--- wp-content/themes/cae/templates/newsletter/newsletters-grid.php
+++ wp-content/themes/cae/templates/newsletter/newsletters-grid.php
@@ -1,17 +1,13 @@
 <?php $work = wp_get_post_terms($post->ID,'work', array("fields" => "names"));
 $campaign = wp_get_post_terms($post->ID,'campaign', array("fields" => "names"));
-$workSlug = wp_get_post_terms($post->ID,'work', array("fields" => "slugs")); ?><li class="grid-item <?php if($workSlug): echo strtolower($workSlug[0]); else: echo 'default'; endif; ?>">
-	<a href="<?php the_permalink(); ?>"<?php if($image_url): ?> class="bg-color hover-trigger" style="background-image:url(<?php echo $image_url; ?>);"<?php endif; ?>>
-			<?php if(!$image_url): ?>
-				<div class="breadcrumbs hover-white">
-					<?php if($work): ?><span class="color hover-white"><?php echo $work[0]; ?></span> <span class="carrot hover-white">></span> <?php endif; ?><?php if($campaign): echo $campaign[0]; endif; ?><strong class="hover-white"><?php echo get_post_type( get_the_ID() ); ?></strong>
-				</div>
-				<h2 class="grid-title hover-white"><?php the_title(); ?></h2>
-				<p class="date hover-white"><?php the_time('F j, Y'); ?></p>
-			<?php else: ?>
-				<div class="bg-color show-on-hover">
-					<h2 class="grid-title hover-white white-text v-centered"><?php the_title(); ?></h2>
-				</div>
-			<?php endif; ?>
+$workSlug = wp_get_post_terms($post->ID,'work', array("fields" => "slugs")); 
+?><li class="grid-item <?php if($workSlug): echo strtolower($workSlug[0]); else: echo 'default'; endif; ?>">
+	<a href="<?php the_permalink(); ?>">	
+	<div class="breadcrumbs hover-white">
+		<?php if($work): ?><span class="color hover-white"><?php echo $work[0]; ?></span> <span class="carrot hover-white">></span> <?php endif; ?><?php if($campaign): echo $campaign[0]; endif; ?><strong class="hover-white"><?php echo get_post_type( get_the_ID() ); ?></strong>
+	</div>
+	<h2 class="grid-title hover-white"><?php the_title(); ?></h2>
+	<p class="date hover-white"><?php the_time('F j, Y'); ?></p>
+			
 	</a>
 </li>
