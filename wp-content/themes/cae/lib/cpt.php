65333357977232b30207fa1e81a59e4d3268ef94
Jon Bukiewicz <jonathan@tenebroso.net>
2015-06-03 22:36:55 -0400

    Add new Homepage Tile CPT

--- wp-content/themes/cae/lib/cpt.php
+++ wp-content/themes/cae/lib/cpt.php
@@ -51,6 +51,7 @@ $postTypes = array(
     'Grant' => '',
     'Video' => '',
     'Infographic' => '',
+    'Custom Homepage Tile' => array('public' => false, 'publicly_queryable' => true, 'show_ui' => true, 'supports' => array( 'title')),
     'Read This Tile' => array('public' => false, 'publicly_queryable' => true, 'show_ui' => true, 'supports' => array( 'title')),
     'Content Map' => array('public' => false, 'publicly_queryable' => true, 'show_ui' => true, 'supports' => array( 'title'))
 );
