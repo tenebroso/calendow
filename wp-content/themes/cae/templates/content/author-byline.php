ad2453f1e8fea5a1581466d239a05ba119e1deaa
Jon Bukiewicz <jonathan@tenebroso.net>
2015-10-19 11:30:00 -0500

    Multiple authors display

--- wp-content/themes/cae/templates/content/author-byline.php
+++ wp-content/themes/cae/templates/content/author-byline.php
@@ -30,3 +30,41 @@
     <?php endif; ?>
 
 <?php endif; ?>
+
+<?php if( have_rows('additional_authors') ): ?>
+ 
+    <?php while( have_rows('additional_authors') ): the_row(); ?>
+ 
+        <?php 
+        
+        $subaPhoto = get_sub_field('author_photo'); 
+        $subaTitle = get_sub_field('author_title');
+        $subaName = get_sub_field('author_name');
+        $subaUrl = get_sub_field('author_url');
+        
+        ?>
+
+        <?php if($subaUrl): ?>
+            <a class="author_byline" href="<?php echo $subaUrl; ?>">
+        <?php else: ?>
+            <div class="author_byline">
+        <?php endif; ?>
+
+        <?php if($subaPhoto): ?>
+            <img src="<?php echo $subaPhoto; ?>" class="author_byline--photo img-circle">
+        <?php endif; ?>
+        
+        <div class="author_byline--details">
+            <p class="author_byline--name"><?php echo $subaName; ?></p>
+            <?php if($subaTitle): ?><p class="author_byline--title"><?php echo $subaTitle; ?></p><?php endif; ?>
+        </div>
+
+        <?php if($subaUrl): ?>
+            </a>
+        <?php else: ?>
+            </div>
+        <?php endif; ?>
+        
+    <?php endwhile; ?>
+ 
+<?php endif; ?>
